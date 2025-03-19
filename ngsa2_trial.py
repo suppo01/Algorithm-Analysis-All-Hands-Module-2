878cf607943262662c7533d519df7eb42d037f3d
suppo01 <suppo01@allegheny.edu>
2025-03-19 19:02:46 -0400

    add binary_tournament example code

--- ngsa2_trial.py
+++ ngsa2_trial.py
@@ -1,1 +1,30 @@
+import numpy as np
+from pymoo.algorithms.soo.nonconvex.ga import GA
+from pymoo.operators.selection.tournament import TournamentSelection
+from pymoo.optimize import minimize
+from pymoo.problems import get_problem
 
+# P is the permutations of things to be compared between competitors
+def binary_tournament(pop, P, _, **kwargs):
+    # The P input defines the tournaments and competitors
+    n_tournaments, n_competitors = P.shape
+
+    if n_competitors != 2:
+        raise Exception("Only pressure=2 allowed for binary tournament!")
+
+    # the result this function returns
+    S = np.full(n_tournaments, -1, dtype=np.int)
+
+    # now do all the tournaments
+    for i in range(n_tournaments):
+        a, b = P[i]
+
+        # if the first individual is better, choose it
+        if pop[a].F < pop[b].F:
+            S[i] = a
+
+        # otherwise take the other individual
+        else:
+            S[i] = b
+
+    return S
